b76cd00036a7eecb30a4604e4074f131be153b55
--global <--global>
2023-12-11 00:24:49 +0300

    javascript homework lesson 2

---
+++ js/main.js
@@ -0,0 +1,258 @@
+// Bolean 1
+
+// let a = +prompt("Son kiriting!");
+
+// let res = a % 2 == 0;
+
+// console.log(res);
+
+// Bolean 2
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+
+// let res = a >= 0 && b <= 2;
+
+// console.log(res);
+
+// Bolean 3
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let c = +prompt("c ni kiriting");
+
+// let res = (a < b && b > c) || (a > b && b < c);
+
+// console.log(res);
+
+// Bolean 4
+
+// let a = prompt("a ni kiriting0");
+// let b = prompt("b ni kiriting0");
+
+// let res = a % 2 == 1 && b % 2 == 1;
+
+// console.log(res);
+
+// Bolean 5
+
+// let a = prompt("a ni kiriting0");
+// let b = prompt("b ni kiriting0");
+
+// let res = a % 2 == 1 || b % 2 == 1;
+
+// console.log(res);
+
+// Bolean 6
+
+// let a = prompt("a ni kiriting0");
+// let b = prompt("b ni kiriting0");
+
+// let res = a % 2 == 1 && b % 2 == 1;
+
+// console.log(res);
+
+// Bolean 7
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let c = +prompt("c ni kiriting");
+
+// let res = a > 0 && b > 0 && c > 0;
+
+// console.log(res);
+
+// Bolean 8
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let c = +prompt("c ni kiriting");
+
+// let res =
+//   (a > 0 && b < 0 && c < 0) ||
+//   (a < 0 && b > 0 && c < 0) ||
+//   (a > 0 && b > 0 && c > 0);
+
+// console.log(res);
+
+// Bolean 9
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let c = +prompt("c ni kiriting");
+
+// let res =
+//   (a > 0 && b > 0 && c < 0) ||
+//   (a < 0 && b > 0 && c > 0) ||
+//   (a > 0 && b < 0 && c > 0);
+
+// console.log(res);
+
+// Bolean 10
+
+// let a = +prompt("a ni kiriting");
+// let res = a >= 10 && a < 100;
+
+// console.log(res);
+
+// Bolean 11
+
+// let a = +prompt("a ni kiriting");
+
+// let res = (a >= 100 && a < 1000) % 2 == 1;
+
+// console.log(res);
+
+// Bolean 12
+
+// Bolean 13
+
+// let abc = +prompt("3 honali son kiriting");
+
+// let a = Math.trunc(abc / 100);
+// let b = Math.trunc((abc / 10) % 10);
+// let c = Math.trunc(abc % 10);
+
+// let res = a < b && b < c;
+
+// console.log(res);
+
+// Bolean 14
+
+// let abc = +prompt("3 honali son kiriting");
+
+// let a = Math.trunc(abc / 100);
+// let b = Math.trunc((abc / 10) % 10);
+// let c = Math.trunc(abc % 10);
+
+// let res = (a < b && b < c) || (a > b && b > c);
+
+// console.log(res);
+
+// Bolean 15
+
+// let abc = +prompt("son kiriting");
+
+// let a = Math.trunc(abc / 100);
+// let b = Math.trunc((abc / 10) % 10);
+// let c = Math.trunc(abc % 10);
+
+// let res = a == b && b == c && a == c;
+
+// console.log(res);
+
+// Ifoda 1
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+
+// let Y1 = 1 / Math.pow(A, 2);
+
+// let Y2 = Math.pow(B / 10, 2);
+
+// let Y3 = Math.sqrt(A + C);
+
+// let Y = Y1 - Y2 * Y3;
+
+// console.log(Y);
+
+// Ifoda 2
+
+// let A = +prompt("A ni kiriting");
+// let B = +prompt("B ni kiriting");
+// let C = +prompt("C ni kiriting");
+// let D = +prompt("D ni kiriting");
+
+// let abcd = ((A + B) / (C + D)) * 2;
+// let Y = abcd + Math.pow(B, 2);
+
+// console.log(Y);
+
+// Ifoda 3
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let d = +prompt("D ni kiriting");
+
+// let abd = (Math.pow(a, d)) + (Math.pow(4 * c) / 3);
+
+// let y = abd * 2;
+
+// console.log(y);
+
+// Ifoda 4
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let d = +prompt("D ni kiriting");
+// let c = +prompt("C ni kiriting");
+
+// let abcd = (1 / Math.pow(a, 2)) * Math.pow(b / 10, 3) * Math.pow(c + d, 2);
+
+// console.log(abcd);
+
+// Ifoda 5
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let d = +prompt("D ni kiriting");
+// let c = +prompt("C ni kiriting");
+
+// let ab = a / b - 1;
+// let cd = c / (d - 1);
+// let abcd = Math.pow(ab / cd, 2);
+
+// console.log(abcd);
+
+// Ifoda 6
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let d = +prompt("D ni kiriting");
+// let c = +prompt("C ni kiriting");
+
+// let cd = c + d / (a + c);
+// let y = (a + b) / cd;
+
+// console.log(y);
+
+// Ifoda 7
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let c = +prompt("C ni kiriting");
+
+// let abc = Math.pow(a + b + c, 2) - Math.pow(a - b - c, 2);
+// let y = Math.pow(abc, 2);
+
+// console.log(y);
+
+// Ifoda 8
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let c = +prompt("C ni kiriting");
+
+// let abc = Math.pow(a, 2 + b, 2 + c, 2);
+// let y = abc / (a * b * c);
+
+// console.log(y);
+
+// Ifoda 9
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let c = +prompt("C ni kiriting");
+// let d = +prompt("D ni kiriting");
+
+// let d3 = Math.pow(d, 3);
+// let c2 = Math.pow(c, 2);
+// let ad = a * (d3 / 3);
+// let bc = b * (c2 / 2);
+// let y = ad + bc;
+
+// console.log(y);
+
+// Ifoda 10
+
